e668d5c7877fb5c7aa7ba7db7e9780c153be019e
Thanh Nguyen <vietthanh090198@gmail.com>
2025-11-07 17:31:04 +0700

    feat: update code

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -113,12 +113,17 @@ function checkAnimations() {
 
 
   // Album images animation
-  images.forEach((img) => {
+  images.forEach((img, index) => {
     const rect = img.getBoundingClientRect();
     if (rect.top < trigger && rect.bottom > topTrigger) {
       img.classList.add("visible");
     } else {
-      img.classList.remove("visible");
+      const isLastTwoImages = index >= images.length - 2;
+      const alreadyVisible = img.classList.contains("visible");
+      if (isMobile && isLastTwoImages && alreadyVisible) {
+      } else {
+        img.classList.remove("visible");
+      }
     }
   });
 }
